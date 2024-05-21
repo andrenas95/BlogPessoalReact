bb3814bc2b52e37c3bd6d2ba337c30e962124d93
Andre <andrenas.nascimento95@gmail.com>
2024-05-20 21:28:52 -0300

    Atividade 5 React Consumindo a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
-  return (
-    <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-);
+    return (
+        <>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Navbar />
+                    <div className='min-h-[80vh]'>
+                        <Routes>
+                            <Route path="/" element={<Login />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/cadastro" element={<Cadastro />} />
+                            <Route path="/login" element={<Login />} />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
+        </>
+    )
 }
-export default App;
+
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,30 +1,40 @@
-
-
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react"
 
 function Navbar() {
- 
-  
 
-  return (
-    <>
-     <div className='w-full text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-4xl font-bold uppercase'>Blog Pessoal
+    const navigate = useNavigate()
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        alert('O usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
+    
+
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-neutral-900 text-white'> 
             
-            </div>
+                <div className="container flex justify-between text-lg">
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
 
-            <div className='flex gap-9'>
-              
-              <div className='hover:underline'>Postagens </div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        <Link to="" onClick={logout} className="hover:underline">Sair</Link>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
